047324c3a9f3bac1d38c9ebd0ea88d899303d642
sagitush <44786027+sagitush@users.noreply.github.com>
2019-09-06 16:18:01 +0300

    Add files via upload

---
+++ Question3/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Question2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Question3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel3 view = new ViewModel3(); 
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            DataContext = view;
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text;
+
+            MyButton.IsEnabled = false;
+
+            string Url = MyTextBox.Text;
+
+            Task.Run(() =>
+            {
+                WebRequest webRequest = WebRequest.Create(Url);
+                WebResponse response = webRequest.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                string size = text.Length.ToString();
+
+                Action uiwork = () =>
+                {
+                    MyTextBlock.Text = size;
+                    MyButton.IsEnabled = true;
+                };
+                    SafeInvoke(uiwork);
+                });
+           
+            }
+   
+        private void SafeInvoke(Action work)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                work.Invoke();
+                return;
+            }
+            this.Dispatcher.BeginInvoke(work);
+        }
+    }
+}
---
+++ Question3/ViewModel3.cs
@@ -0,0 +1,97 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Question2
+{
+   public class ViewModel3:INotifyPropertyChanged
+    {
+        private bool canexecute = true;
+        private string size;
+        public string Size
+        {
+            get
+            {
+                return this.size;
+            }
+            set
+            {
+                size = value;
+                OnPropertyChanged("Size");
+            }
+        }
+        private string url;
+        public string URL
+        {
+            get
+            {
+                return this.url;
+            }
+            set
+            {
+                url = value;
+                OnPropertyChanged("URL");
+            }
+        }
+        
+        public DelegateCommand MyDelegate { get; set; }
+
+        public ViewModel3()
+        {    
+            
+            MyDelegate = new DelegateCommand(ExecuteCommand,CanExecute);
+
+            Size = "Please wait...";
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    MyDelegate.RaiseCanExecuteChanged();
+                    Thread.Sleep(500);
+                }
+
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public bool CanExecute()
+        {
+            return canexecute;
+        }
+
+        private async void ExecuteCommand()
+        {
+            canexecute = false;
+            string text;
+            await Task.Run(() =>
+            {              
+                WebRequest webRequest = WebRequest.Create(URL);
+                WebResponse response = webRequest.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    text = reader.ReadToEnd();
+                }
+                Size = text.Length.ToString();
+            });
+            canexecute = true;
+        }
+    }
+}
