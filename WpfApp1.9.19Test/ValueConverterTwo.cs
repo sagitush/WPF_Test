5be45870c7544eea0c038077c18def43e471311e
sagitush <44786027+sagitush@users.noreply.github.com>
2019-09-06 16:23:15 +0300

    Add files via upload

---
+++ WpfApp1.9.19Test/ValueConverterTwo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1._9._19Test
+{
+    class ValueConverterTwo : IValueConverter
+    {
+        int minimumSize = 29;
+        int maximumSize = 129;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int SizeOfSelection = System.Convert.ToInt32(value);
+
+            if (SizeOfSelection <= minimumSize + 25)
+            {
+                return "SMALL";
+            }
+            else if (SizeOfSelection <= minimumSize + 50)
+            {
+                return "MEDIUM";
+            }
+            else if (SizeOfSelection <= minimumSize + 75)
+            {
+                return "LARGE";
+            }
+            else if (SizeOfSelection <= maximumSize)
+            {
+                return "EXTRA LARGE";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp1.9.19Test/ViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1._9._19Test
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int heightbtn;
+        public int HeightBTN
+        {
+            get
+            {
+                return this.heightbtn;
+            }
+            set
+            {
+                heightbtn = value;
+                OnPropertyChanged("HeightBTN");
+            }
+        }
+
+        private int widthbtn;
+        public int WidthBTN
+        {
+            get
+            {
+                return this.widthbtn;
+            }
+            set
+            {
+                widthbtn = value;
+                OnPropertyChanged("WidthBTN");
+            }
+        }
+
+        private string textbtn;
+        public string TextBTN
+        {
+            get
+            {
+                return this.textbtn;
+            }
+            set
+            {
+                textbtn = value;
+                OnPropertyChanged("TextBTN");
+            }
+        }
+
+        public DelegateCommand MyDelegate { get; set; }
+
+        public ViewModel()
+        {
+            MyDelegate = new DelegateCommand(ExecuteCommand);
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private void ExecuteCommand()
+        {
+            MonitorWindow mw = new MonitorWindow(this);
+            mw.Show();
+        }
+    }
+   
+}
